b29e523eb2046be033a17361134218618b09d533
Mitchell Saremba <mitchellsaremba@proton.me>
2025-05-15 11:36:15 -0700

    fix: Removed mobile menu overflow

--- client/portfolio/src/components/nav/MobileMenu.tsx
+++ client/portfolio/src/components/nav/MobileMenu.tsx
@@ -2,7 +2,7 @@ import Navlink from './Navlink';
 
 export default function MobileMenu() {
   return (
-    <div id='mobile-menu' className='sm:hidden w-full h-full bg-slate-800 text-white flex flex-col'>
+    <div id='mobile-menu' className='sm:hidden w-full bg-slate-800 text-white flex flex-col gap-3'>
       <Navlink href='#hero' children='Home' />
       <Navlink href='#projects' children='Projects' />
       <Navlink href='#skills' children='Skills' />
